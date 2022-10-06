598e440eb71d810b6060a601d1656d5f2aaec10b
Hinda-Abdullahi <99586014+HindaAbdullahi@users.noreply.github.com>
2022-10-06 12:31:36 +0300

    Update r.js

--- routes/r.js
+++ routes/r.js
@@ -1,1 +1,17 @@
 
+const {
+    singleDepartment,
+    getDepartment,
+    updateDepartment,
+    deleteDepartment,
+  } = require("../controllers/department");
+  
+  const router = require("express").Router();
+  
+ 
+  router.get("/apartments", singleDepartment);
+  router.post("/apartments", getDepartment);
+  router.put("/apartments/:apartid",updateDepartment);
+  router.delete("/apartments/:apartid",  deleteDepartment);
+  
+  module.exports = router;
