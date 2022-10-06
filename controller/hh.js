0475eda5a41a30f330f48b5ff8ec8cca3e42a599
Hinda-Abdullahi <99586014+HindaAbdullahi@users.noreply.github.com>
2022-10-06 12:28:33 +0300

    Update hh.js

--- controller/hh.js
+++ controller/hh.js
@@ -1,1 +1,54 @@
+const express = require("express");
+const auth = require("../middleware/auth");
+const validateObjectID = require("../middleware/objectdId");
+const validator = require("../middleware/validate");
+
+const { validate, Department } = require("../models/department");
+const singleDepartment = async (req, res) => {
+
+    const dept = await Department.find({});
+    res.send(dept);
+};
+const getDepartment = async (req, res) => {
+    let dept = await Department.findOne({ name: req.body.name });
+    if (dept) return res.status(400).send("department already registered");
+
+    dept = await Department.create({
+        name: req.body.name,
+        manager: req.body.manager,
+        phone: req.body.phone,
+    });
+
+    res.send(dept);
+}
+const updateDepartment = async (req, res) => {
+    let dept = await Department.findOne({ name: req.body.name });
+    if (dept) return res.status(400).send("department already registered");
+
+    dept = await Department.findByIdAndUpdate(
+        req.params.id,
+        {
+            name: req.body.name,
+            manager: req.body.manager,
+            phone: req.body.phone,
+        },
+        { new: true }
+    );
+
+    res.send(dept);
+}
+
+const deleteDepartment = async (req, res) => {
+
+    const dept = await Department.findByIdAndRemove(req.params.id);
+    if (!dept) return res.status(404).send("invalid delete");
+
+    res.send(dept);
+}
+module.exports = {
+    singleDepartment,
+    getDepartment,
+    updateDepartment,
+    deleteDepartment,
+};
 
