c4fac1c8e080c039078eb6f75c348b0124d34289
Hinda-Abdullahi <99586014+HindaAbdullahi@users.noreply.github.com>
2022-10-06 11:24:32 +0300

    Update apart.js

--- apartment/controller/apart.js
+++ apartment/controller/apart.js
@@ -1,1 +1,46 @@
 
+router.get("/", async (req, res) => {
+  const apartment = await Apartment.find({});
+  res.send(apartment);
+});
+
+router.post("/", [validator(validate)], async (req, res) => {
+  let apartment = await Apartment.findOne({ name: req.body.name });
+  if (apartment) return res.status(400).send("apartment all ready registered");
+  apartment = await Apartment.create({
+    name: req.body.name,
+    location: req.body.location,
+    address: req.body.address,
+  });
+
+  res.send(apartment);
+});
+
+router.put(
+  "/:id",
+  [validateObjectID, validator(validate)],
+  async (req, res) => {
+    let apartment = await Apartment.findOne({ name: req.body.name });
+    if (apartment)
+      return res.status(400).send("apartment all ready registered");
+    apartment = await Apartment.findByIdAndUpdate(
+      req.params.id,
+      {
+        name: req.body.name,
+        location: req.body.location,
+        address: req.body.address,
+      },
+      { new: true }
+    );
+
+    res.send(apartment);
+  }
+);
+
+router.delete("/:id", [validateObjectID], async (req, res) => {
+  const apartment = await Apartment.findByIdAndRemove(req.params.id);
+
+  res.send(apartment);
+});
+
+module.exports = router;
